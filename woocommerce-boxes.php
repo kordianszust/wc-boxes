2801f149a188ddeb9f14353470263d50d3020286
Kordian Szust <k.szust@it360.pl>
2023-01-11 18:54:01 +0100

    Plugin init file

---
+++ woocommerce-boxes.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: Woocommerce boxes
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Woocommerce boxes extensions
+ * Version:           0.1
+ * Requires PHP:      7.2
+ * Author:            Kordian Szust
+ */
+
+?>
